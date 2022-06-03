ad18c1b3d4f65671d7d8d93cf36fd619fbdbae50
Andrew Lalis <andrewlalisofficial@gmail.com>
2022-06-03 14:56:23 +0200

    Set to always allow any origin to open app websocket.

--- src/main/java/nl/andrewl/railsignalapi/live/websocket/WebsocketConfig.java
+++ src/main/java/nl/andrewl/railsignalapi/live/websocket/WebsocketConfig.java
@@ -3,13 +3,10 @@ package nl.andrewl.railsignalapi.live.websocket;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistration;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
-import java.util.Set;
 
 /**
  * Configuration for Rail Signal's websockets. This includes both app and
@@ -24,7 +21,6 @@ public class WebsocketConfig implements WebSocketConfigurer {
 	private final ComponentWebsocketHandshakeInterceptor componentInterceptor;
 	private final AppWebsocketHandler appHandler;
 	private final AppWebsocketHandshakeInterceptor appInterceptor;
-	private final Environment env;
 
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
@@ -33,12 +29,6 @@ public class WebsocketConfig implements WebSocketConfigurer {
 				.addInterceptors(componentInterceptor);
 		WebSocketHandlerRegistration appHandlerReg = registry.addHandler(appHandler, "/api/ws/app/*")
 				.addInterceptors(appInterceptor);
-//		appHandlerReg.setAllowedOrigins("*");
-		// If we're in a development profile, allow any origin to access the app websocket.
-		// This is so that we can use a standalone JS dev server.
-		if (Set.of(env.getActiveProfiles()).contains("development")) {
-			log.info("Allowing all origins to access app websocket because development profile is active.");
-			appHandlerReg.setAllowedOrigins("*");
-		}
+		appHandlerReg.setAllowedOrigins("*");
 	}
 }
